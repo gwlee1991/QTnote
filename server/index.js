ed5beeef8bf72a09f243400cb2acaa3de760fe7b
Geewon Lee <gwlee1991@gmail.com>
2017-11-11 18:15:13 -0800

    use cookie to serialize and deserialize user

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
 require("./models/User");
 require("./services/passport");
 
-const app = express();
-
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/nevernote");
-mongoose.connection.once("open", () => console.log("good to go"));
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require("./routes/authRoutes")(app);
 
