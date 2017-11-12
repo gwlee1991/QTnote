6acd6b2140a0585aded201e5e967c64368b4145f
Geewon Lee <gwlee1991@gmail.com>
2017-11-11 17:09:44 -0800

    connect to mongodb using mongoose and create User model to create an instance of the collection when authenticating

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('./services/passport');
 
+require("./models/User");
+require("./services/passport");
 
 const app = express();
 
-require("./routes/authRoutes")(app);
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/nevernote");
+mongoose.connection.once("open", () => console.log("good to go"));
 
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+// same of Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
