1180b6eaf972b94dd0798a1507859bf8c37a0a40
Geewon Lee <gwlee1991@gmail.com>
2017-12-13 16:23:28 -0800

    header styling and add logo image

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -8,29 +8,34 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Login With Google</a></li>;
+        return <li>
+            <a href="/auth/google">
+              <p className="bttn-signin"><i className="fa fa-google-plus" aria-hidden="true" />  Login with Google</p>
+            </a>
+          </li>;
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return <li><a href="/api/logout"><p className="bttn-signin">Logout</p></a></li>;
     }
   }
   
   render(){
-    return(
-      <nav className="nav">
+    return <nav className="nav">
         <div className="nav-wrapper">
-          <div className="nav-items nav-right">
-            <Link to={this.props.auth ? '/main' : '/' } className="logo">
-              Nevernote
+          <div className="nav-items nav-left">
+            <Link to={this.props.auth ? "/main" : "/"}>
+              <div className="logo">
+                <div className="logo-img-container">
+                  <img className="logo-img" alt="logo" src="https://res.cloudinary.com/dbtdkqyeo/image/upload/v1513210193/nevernote_umqrul.png" />
+                </div>
+                <div className="logo-txt">NEVERNOTE</div>
+              </div>
             </Link>
           </div>
-          <div className="nav-items nav-left">
-            <ul className="session-buttons">
-              {this.renderContent()}
-            </ul>
+          <div className="nav-items nav-right">
+            <ul className="session-buttons">{this.renderContent()}</ul>
           </div>
         </div>
-      </nav>
-    )
+      </nav>;
   }
 }
 
