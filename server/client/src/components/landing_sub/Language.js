baaea1d8d12cf232bbabd4335d30a9b46f7d67d8
Geewon Lee <gwlee1991@gmail.com>
2017-12-14 20:25:51 -0800

    finish landing component styling except for text-font

--- server/client/src/components/landing_sub/Language.js
+++ server/client/src/components/landing_sub/Language.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import '../../css/landing_sub/language.css';
 
 class Language extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showOptions: false,
+      language: "English (US)"
+    };
+    this.toggleShowOptions = this.toggleShowOptions.bind(this);
+  }
+
+  toggleShowOptions(){
+    this.setState({ showOptions: !this.state.showOptions})
+  }
+
+  renderLanguageOptions(){
+    const languages = ["English (US)", "한국어"]
+    return(
+      <div className="language-selection-container">
+        <ul>
+        {languages.map(language => (
+          <li className="language-option" key={languages.indexOf(language)}>{language}</li>
+        ))}
+        </ul>
+      </div>
+    )
+  }
+  
   render(){
     return (
-      <div>
-        Language Component
+      <div className="language-container">
+        <p className="language-selection" onClick={this.toggleShowOptions}>Choose a language: {this.state.language} <i className="fa fa-caret-down" aria-hidden="true" /></p>
+        {this.state.showOptions ? this.renderLanguageOptions(): ""}
       </div>
     )
   }
--- server/client/src/components/landing_sub/SocialProof.js
+++ server/client/src/components/landing_sub/SocialProof.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import '../../css/landing_sub/socialproof.css';
+
 
 class SocialProof extends Component {
   render(){
-    return (
-      <div>
-        SocialProof Component
-      </div>
-    )
+    return <div className="sp-container">
+        <div className="sp-heading">
+          <h3>
+            A few people rely on Nevernote to stay organized and get
+            more done. Find out why.
+          </h3>
+        </div>
+        <div className="create-profile-btn-container">
+          <button className="create-account-bttn">
+            <a href="/auth/google">Sign up for free</a>
+          </button>
+        </div>
+      </div>;
   }
 }
 
